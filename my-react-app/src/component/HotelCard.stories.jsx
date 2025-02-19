ac1f018ce02285b28799d127dfc746a871729847
Anushri2831 <anushrichinchankar5gmail.com>
2025-02-19 12:14:20 +0530

    initial-commit

---
+++ my-react-app/src/component/HotelCard.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import HotelCard from "./HotelCard";
+
+export default {
+  title: "Components/HotelCard",
+  component: HotelCard,
+};
+
+export const Default = () => <HotelCard />;
